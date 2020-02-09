1e7ba5165c6f57afdc2a4905f572348ae5f9e511
chrharr1 <chrharr1@uat.edu>
2020-02-09 12:51:57 -0700

    fresh

--- Source.cpp
+++ Source.cpp
@@ -10,83 +10,139 @@ using namespace std;
 int main()
 
 {
-	cout << "\t\t\tWelcome to Word Jumble!\n\n";
-	cout << "Unscramble the letters to make a word.\n";
+	//play again
+
 	bool playAgain = false;
 
+	//For play again
 
-	bool usersPlayAgainChoice;
-	// make user do 3 rounds for 1 game
-	for (int i = 0; i < 3; ++i)
+	char usersPlayAgainChoice;
+
+	cout << "\t\t\tWelcome to Word Jumble!\n\n";
+
+	cout << "Unscramble the letters to make a word.\n";
+
+	cout << "Enter 'hint' for a hint.\n";
+
+	cout << "Enter 'quit' to quit the game.\n\n";
+	for (int i = 0; i < 3; i++)
 	{
 		cout << "\nWelcome to round " << i + 1 << " of 3.\n\n";
 	}
 
-	enum fields { Word, Hint, Num_Fields };
-	const int Num_Words = 5;
-	const string Words[Num_Words][Num_Fields] =
-	{
-		{	"return", "Ends the code."} ,
+
+	do {
+
+		for (int i = 0; i < 3; i++) {
+
+			//Words and hints
+
+			enum fields { WORD, HINT, NUM_FIELDS };
+			const int NUM_WORDS = 10;
+			const string WORDS[NUM_WORDS][NUM_FIELDS] =
+
+			{
+
+			{	"return", "Ends the code."} ,
 	{"false", "Something that is not right."} ,
 	{"bool", "It can be true or false depending on the value of i."},
 	{"Try", "one word for attempting to do something."} ,
 		{"jumble", "It's what the game is all about."}
-	};
-	srand(static_cast<unsigned int>(time(0)));
-	int choice = (rand() % Num_Words);
-	string theWord = Words[choice][Word];
-	//word to guess
-	string theHint = Words[choice][Hint];
-	//hint for word
 
-	string jumble = theWord;
-	//jumbled version of word
-	int length = jumble.size();
-	for (int i = 0; i < length; ++i)
-	{
-		int index1 = (rand() % length);
-		int index2 = (rand() % length);
-		char temp = jumble[index1];
-		jumble[index1] = jumble[index2];
-		jumble[index2] = temp;
-	}
+			};
 
-	cout << "Enter 'hint' for a hint.\n";
-	cout << "Enter 'quit' to quit the game.\n\n";
-	cout << "The jumble is: " << jumble;
+			//Functions needed for jumble to happen
 
-	string guess;
-	cout << "\n\nYour guess: ";
-	cin >> guess;
+			srand(static_cast<unsigned int>(time(0)));
 
-	while ((guess != theWord) && (guess != "quit"))
-	{
-		if (guess == "hint")
-		{
-			cout << theHint;
+			int choice = (rand() % NUM_WORDS);
+
+			string theWord = WORDS[choice][WORD];  // word to guess
+			string theHint = WORDS[choice][HINT];  // hint for word
+			string jumble = theWord;  //Jumbled version of word
+			int length = jumble.size();
+
+			//Loop for the game to jumble the words
+			for (int i = 0; i < length; ++i)
+
+			{
+
+				int index1 = (rand() % length);
+				int index2 = (rand() % length);
+				char temp = jumble[index1];
+				jumble[index1] = jumble[index2];
+				jumble[index2] = temp;
+			}
+
+
+
+			cout << "The jumble is: " << jumble;
+
+			//player's guess
+
+			string guess;
+
+			cout << "\n\nYour guess: ";
+
+			cin >> guess;
+
+			//guessing the word and asking for hints
+
+			while ((guess != theWord) && (guess != "quit"))
+
+			{
+				if (guess == "hint")
+				{
+					cout << theHint;
+				}
+				else
+				{
+					cout << "Sorry, that's not it.";
+				}
+				cout << "\n\nYour guess: ";
+				cin >> guess;
+			}
+			if (guess == theWord)
+			{
+				cout << "\nThat's it!  You guessed it!\n";
+			}
+
 		}
+
+
+
+		cout << "Would you like to play again? (Y/N)" << endl;
+
+		cin >> usersPlayAgainChoice;
+
+		//user input to keep playing
+
+		if (usersPlayAgainChoice == 'y' || usersPlayAgainChoice == 'Y')
+
+		{
+
+			playAgain = true;
+
+		}
+
 		else
+
 		{
-			cout << "Sorry, that's not it.";
+
+			playAgain = false;
+
 		}
-		cout << "\n\nYour guess:";
-		cin >> guess;
-	}
-	if (guess == theWord)
-	{
-		cout << "\nThat's it! You guessed it!\n";
-	}
+
+		//get out of loop
+
+	} while (playAgain = true);
 
 
-	cout << "\nYou have completed 3 rounds and the game is over!\n";
-	cout << "\nWould you like to play again? (y/n) ";
-	cin >> usersPlayAgainChoice;
 
-	while (usersPlayAgainChoice == 'y' || usersPlayAgainChoice == 'y');
+	cout << "\nThanks for playing.\n";
 
 
-		cout << "\nThanks for playing. \n";
-		return 0;
-	
+
+	return 0;
+
 }
-
